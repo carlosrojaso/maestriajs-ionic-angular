d67a78001b8983faefe8f5c439e430e06e0f6fae
Carlos Rojas <carlos.rojas@localhost>
2019-12-22 11:45:42 -0500

    adding dummy data + using directive

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
+import { DummyData } from '../dummy-tasks';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  items: any = [];
+  constructor() {
+    this.items = DummyData;
+  }
 
 }
